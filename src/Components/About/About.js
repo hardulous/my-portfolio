edbc2638bd16c96746b9700e62345ea16f111928
amanbisht07 <aman@costacloud.com>
2025-01-15 17:27:43 +0530

    added experience

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import S from "./About.module.css"
-import {FaAward} from "react-icons/fa"
-import {FaRegBuilding} from "react-icons/fa"
-import {VscFolderLibrary} from "react-icons/vsc"
-import img from "../../assets/about-pic2.jpg"
+import React from "react";
+import S from "./About.module.css";
+import { FaAward } from "react-icons/fa";
+import { FaRegBuilding } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
+import img from "../../assets/about-pic2.jpg";
 const About = () => {
   return (
-    
     <section id="about">
-
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
@@ -20,34 +18,37 @@ const About = () => {
         </div>
         <div className={`${S.aboutContent}`}>
           <div className={`${S.aboutCards}`}>
-
             <article className={`${S.aboutCard}`}>
-              <FaAward className={`${S.aboutIcon}`}/>
+              <FaAward className={`${S.aboutIcon}`} />
               <h5>Experience</h5>
-              <small>2+ Years Working Experience</small>
+              <small>3 Years Of Experience</small>
             </article>
             <article className={`${S.aboutCard}`}>
-              <FaRegBuilding className={`${S.aboutIcon}`}/>
+              <FaRegBuilding className={`${S.aboutIcon}`} />
               <h5>Industry</h5>
               <small>Have experience of real world projects</small>
             </article>
             <article className={`${S.aboutCard}`}>
-              <VscFolderLibrary className={`${S.aboutIcon}`}/>
+              <VscFolderLibrary className={`${S.aboutIcon}`} />
               <h5>Projects</h5>
               <small>5+ Completed</small>
             </article>
-
           </div>
 
-          <p>Highly skilled and motivated JavaScript Developer with 2+ years of experience in designing and developing enterprise-level applications. Seeking a challenging position in a company to leverage my expertise in JavaScript, React, Next.js, TypeScript and various frameworks to contribute to the company's success.</p>
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-
+          <p>
+            Software Engineer with 3 years of experience building responsive web
+            applications using React, Next.js, and TypeScript. Skilled in
+            leading developer teams, collaborating on projects, and creating
+            user-friendly solutions that deliver excellent performance and great
+            digital experiences
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
-
     </section>
+  );
+};
 
-  )
-}
-
-export default About
+export default About;
